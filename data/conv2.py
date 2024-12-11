d78b064724becfe51618a32d5db3fb27a478e607
Kangurur <olipol2005@gmail.com>
2024-12-11 23:43:06 +0100

    smaug p1 uwu

--- data/conv2.py
+++ data/conv2.py
@@ -1,7 +1,7 @@
-files1=list(["data/uj_raw.txt","data/uj_raw2.txt","data/uj_raw3.txt","data/uj_raw4.txt","data/uj_raw5.txt","data/uj_raw+.txt",])
-files2=list(["data/nieuj_raw.txt","data/nieuj_raw2.txt","data/nieuj_raw3.txt","data/nieuj_raw4.txt","data/nieuj_raw5.txt"])
+files1=list(["data/uj_val.txt"])
+files2=list(["data/nieuj_val.txt"])
 
-with open('data/uj.csv','w') as f:
+with open('data/uj2.csv','w') as f:
     #f.write("\"labels\",\"text\"\n")
     for file in files1:
         with open(file,'r') as g:
@@ -21,10 +21,10 @@ with open('data/uj.csv','w') as f:
                 f.write(line+'\n')
 
 import random
-with open('data/uj.csv','r') as f:
+with open('data/uj2.csv','r') as f:
     data=f.readlines()
     random.shuffle(data)
-    with open('data/uj.csv','w') as g:
+    with open('data/uj2.csv','w') as g:
         g.write("\"labels\",\"text\"\n")
         for line in data:
             g.write(line)
---
+++ transformers/train4.py
@@ -0,0 +1,69 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
+from datasets import Dataset, DatasetDict, load_dataset
+#import pandas as pd
+
+dataset = load_dataset("data/uj+")
+model_name = "dkleczek/bert-base-polish-cased-v1"  # Polish BERT
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+def tokenize_function(examples):
+    return tokenizer(examples["text"], padding="max_length", truncation=True)
+
+tokenized_dataset = dataset.map(tokenize_function, batched=True)
+
+model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
+
+
+training_args = TrainingArguments(
+    output_dir="./smaug_part1",           # Gdzie zapisać wyniki
+    evaluation_strategy="epoch",     # Ewaluacja po każdej epoce
+    learning_rate=2e-5,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
+    num_train_epochs=10,
+    weight_decay=0.01,
+    #logging_dir="./logs",
+    #logging_steps=10,
+    #save_steps=10,
+    #save_total_limit=2,
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+)
+
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_dataset["train"],
+    eval_dataset=tokenized_dataset["validation"],
+    tokenizer=tokenizer,
+)
+
+
+trainer.train()
+
+
+#results = trainer.evaluate()
+#print("Ewaluacja:", results)
+
+model.to("cuda")
+
+from huggingface_hub import login
+write_key = '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' 
+login(write_key)
+hf_name="olipol"
+id=hf_name+"/smaug_part1"
+
+model.push_to_hub(id)
+trainer.push_to_hub(id)
+
+def predict(sentence):
+    inputs = tokenizer.encode(sentence, return_tensors="pt").to("cuda")
+    outputs = model(inputs)
+    probs = outputs.logits.softmax(dim=-1)
+    return "UJ" if probs[0][1] > probs[0][0] else "nie UJ"
+
+n=int(input())
+for i in range(n):
+    text = input()
+    print(predict(text))
